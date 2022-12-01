16c88fd42308b489295417bb6457469406571ae8
LeoMikle <119604830+LeoMikle@users.noreply.github.com>
2022-12-01 17:06:56 +0300

    Add files via upload

---
+++ company.php
@@ -0,0 +1,198 @@
+﻿
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta name="author" content="Leo-Design">
+		<meta name="description" content="GRS GROUP - продажа пиломатериалов, палетов, пилетов, бруса, терасной и фасадной доски, фанера ламинированная, топливные брикеты, евровагонка и многое другое. Доставка по России, Турции, Европе. Материал сосна, ель, лиственница.">
+		<meta http-equiv="content-language" content="ru">
+		<meta http-equiv="Contencopyrightt-Type" content="text/html; content=UTF-8" />
+		<meta name="keywords" content="пиломатериалы, палеты, брус, доска, фанера, ОСП, евровагонка, поддоны, топливные брикеты" />
+			<link rel="stylesheet" type="text/css" href="style.css" />
+			<link href="img\soc\01x.png" rel="shortcut icon" type="img\soc" />
+		<title>GRS GROUP - Пилометариалы по всей России, Турции и Европе.</title>
+			<script src="jquery-1.7.2.min.js"></script>
+			<script src="script.js"></script>
+			
+	</head>
+	<body>
+	
+		<header>
+			<div id=FonShap>
+				<div class=fonshap_cont_soc>
+					<div class=fonshap_cont_soc_img>
+						<a href="https://wa.me/+905065423384" title="whatsapp"><img src="image\icon\ic_soc_01.png" alt="Написать в WatsApp" title="Написать в WatsApp"></a>
+					</div>
+					<div class=fonshap_cont_soc_img>
+						<a href="https://t.me/" title="telegram"><img src="image\icon\ic_soc_02.png" alt="Написать в Telegram" title="Написать в Telegram"></a>
+					</div>
+					<div class=fonshap_cont_soc_img>
+						<a href="mailto:GRSGroup111@gmail.com" title="email"><img src="image\icon\ic_soc_03.png" alt="Написать на Email" title="Написать на Email"></a>
+					</div>
+				</div>
+				<div class=fonshap_cont_nomber>
+					<div class=fonshap_cont_nomber_img>
+						<img src="image\icon\ic_str_01.png" alt="Написать в WatsApp" title="Написать в WatsApp">
+					</div>
+					<div class=fonshap_cont_nomber_txt>
+						<a href="tel:+905065423384" title="Телефон">+90(506)5423384</a>
+					</div>					
+				</div>
+				<div class=fonshap_cont_nomber>
+					<div class=fonshap_cont_nomber_img>
+						<img src="image\icon\ic_str_02.png" alt="Написать в WatsApp" title="Написать в WatsApp">
+					</div>
+					<div class=fonshap_cont_nomber_img>
+						<img src="image\icon\ic_str_03.png" alt="Написать в WatsApp" title="Написать в WatsApp">
+					</div>
+					<div class=fonshap_cont_nomber_txt>
+						<a href="tel:+905065423384" title="Телефон">+90(506)5423384</a>
+					</div>							
+				</div>
+				<div class=fonshap_cont_grafic>
+					<div class=fonshap_cont_grafic_txt>
+						<p>Пн-Пт: 08.00 - 18.00</p>
+					</div>							
+				</div>
+				<div class=fonshap_cont_email>
+					<div class=fonshap_cont_email_txt>
+						<a href="mailto:GRSGroup111@gmail.com" title="email">GRSGroup111@gmail.com</a>
+					</div>							
+				</div>	
+			</div>
+		</header>
+		
+		<article>
+			<div id=FonWhite>
+				<div class=fonwhite_menu_log_img>
+					<a href="index.php" title="На главную"><img src="image\img\im_01.png" alt="GRS GROUP" title="GRS GROUP"></a>
+				</div>
+				<div class=fonwhite_menu_log_txt>
+					<p>GRS GROUP</p>
+				</div>
+				<div class=fonwhite_menu_log_min>
+					<div class=fonwhite_menu_log_min_01>
+						<a href="#Kontakti" title="Контакты">Контакты</a>
+					</div>
+					<div class=fonwhite_menu_log_min_01>
+						<a href="/index.php#Product" title="Наша продукция">Наша продукция</a>
+					</div>	
+					<div class=fonwhite_menu_log_min_01>
+						<a href="index.php" title="О компании">На главную</a>
+					</div>						
+				</div>				
+			</div>
+
+			<div id=FonMain>
+				<div class=fonmain_back_obl_1180>
+					<div class=fonmain_back_big>
+						<img src="image\img\im_03.jpg" alt="Доска половая" title="
+						Доска половая">
+					</div>
+					<div class=fonmain_back_low>
+						<img src="image\img\im_04.jpg" alt="Пиломатериалы" title="
+						Пиломатериалы">
+					</div>
+					<div class=fonmain_back_mid>
+						<img src="image\img\im_02.jpg" alt="Европоддоны" title="
+						Европоддоны">
+					</div>
+				</div>
+			</div>
+			
+			<div id=Razdel_01>
+				<span>О КОМПАНИИ</span>
+				<p>Почему наши пиломатериалы лучше аналогов? Изделия из древесины естественной влажности в процессе использования подсыхают неравномерно. Уменьшается толщина и ширина доски, ее может скрутить «винтом» или «саблей». В строительстве это грозит изменением формы конструкции, появлением зазоров и щелей. В дизайне ведет к нарушению целостности и гладкости напольной, потолочной и настенной облицовки или ее отдельных фрагментов. Мы продаем древесину камерной сушки с влажностью не больше 12-14%. Такие изделия сохраняют начальные размеры, не дают усадки, не коробятся и не растрескиваются во время монтажа и последующей эксплуатации.</p>
+			</div>
+			<div id=Razdel_02>
+				<div class=Razdel_02_img>
+				</div>
+				<div class=Razdel_02_txt>
+					<div class=Razdel_02_txt_500>
+						<li>Мы предлагаем высококачественную продукцию по прямым ценам производителя и комфортные условия для сотрудничества. </li>
+						<li>При заказе через сайт действуют скидки, предоставляется бесплатная доставка. </li>
+						<li>Неиспользованные изделия, сохранившие аккуратный вид, принимаются обратно в течение полугода с момента приобретения. </li>
+					</div>
+				</div>				
+			</div>
+			<div id=Razdel_03>
+				<span>ЧТО МЫ МОЖЕМ ПРЕДЛОЖИТЬ?</span>
+				<p>В зависимости от поставленной цели и специфики работ можно заказать: </p>
+				<li>Вагонку штиль из ангарской сосны и лиственницы. Экологически чистый отделочный материал для интерьерных решений. </li>
+				<li>Прямой или скошенный планкен для отделки фасадов и внутренней облицовки помещений.  </li>
+				<li>Паркетную доску. Прочный облицовочный материал для отделки полов, способный выдерживать внушительные нагрузки, перепады климатических условий.  </li>
+				<li>Палубную и террасную доску. Безопасный и долговечный облицовочный материал для пола, который востребован при обустройстве лоджий, балконов, террас, детских и спортивных площадок, пирсов.  </li>
+				<li>Мебельный щит и другие пиломатериалы из лиственницы.  </li>
+				<li>Пиломатериалы разного профиля и назначения активно используют для каркасного строительства, внешней и внутренней облицовки жилых и коммерческих строений, изготовления предметов мебели, отделки саун, бань, террас, мансард и прочих помещений. При обустройстве территорий его применяют для создания оригинальных заборов, декоративных ограждений, пергол, внутренних дорожек, элементов ландшафтного дизайна, причалов и площадок вокруг бассейнов. </li>
+			</div>		
+			<div id=Razdel_04>
+				<div class=Razdel_04_video>
+				<iframe width="520" height="400" src="https://www.youtube.com/embed/46IK6yK3HkM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+				</div>
+			</div>
+			<div id=Razdel_05>
+				<div class=Razdel_05_part1>
+					<span>ПОЧЕМУ СТОИТ ПОКУПАТЬ МЕТАРИАЛЫ У НАС?</span>
+					<div class=Razdel_05_part1_blockleft>
+						<div class=Razdel_05_part1_blockleft_txt>
+							<p>Наша компания и интернет-магазин – надежный поставщик и ответственный партнер. Мы ценим клиентов и делаем все, чтобы вы купили пиломатериалы по всему миру по цене производителя за 1 куб и с максимальным удобством!</p>
+						</div>		
+						<div class=Razdel_05_part1_blockleft_img>
+							<img src="image\img\im_onas_01.png" alt="наши склады" title="
+						наши склады">
+						</div>						
+					</div>
+				</div>	
+				<div class=Razdel_05_part2>
+					<div class=Razdel_05_part2_blockleft>
+						<div class=Razdel_05_part2_blockleft_img>
+							<img src="image\img\im_onas_02.png" alt="наши склады" title="
+						наши склады">
+						</div>	
+						<div class=Razdel_05_part2_blockleft_txt>
+							<p>Мы работаем оперативно и привозим партию пиломатериала от проверенных производителей в короткие сроки. Даже при высокой загрузке автопарка процесс доставки занимает не более 1-3 дней.</p>
+						</div>						
+					</div>
+				</div>				
+			</div>
+			<div id=Razdel_06>
+				<span>ЗАКАЗЧИКАМ МЫ ПРЕДОСТАВЛЯЕМ:</span>
+				<li>обширный каталог с удобными разделами; привлекательную цену на пиломатериалы всех типов;  </li>
+				<li>четыре варианта оплаты наличными и по безналичному расчету;</li>
+				<li>после оформления заказа не придется неделями ждать получения товара. </li>
+			</div>
+		
+		<div id="foot">
+			<div class=footer_box>
+				<div class=footer_box_01>
+					<a name="Kontakti">
+					<a href="index.php" class=main>на главную</a>
+					<a href="/index.php#Product" class=spisok>фанера ламинированная</a>
+					<a href="/index.php#Product" class=spisok>ориентированно-стружечная плита</a>
+					<a href="/index.php#Product" class=spisok>древесноволокнистая плита</a>
+					<a href="/index.php#Product" class=spisok>евровагонка</a>
+					<a href="/index.php#Product" class=spisok>половая доска</a>
+					<a href="/index.php#Product" class=spisok>терассная доска</a>
+					<a href="/index.php#Product" class=spisok>поддоны и европоддоны</a>
+					<a href="/index.php#Product" class=spisok>пеллеты рассыпные</a>
+					<a href="/index.php#Product" class=spisok>топливные брикеты</a>
+					<p class=brend>GRS GROUP</p>
+				</div>
+				<div class=footer_box_02>
+					<p class=tel1>+90(506)5423384</p>
+					<p class=tel2>+90(506)5423384</p>
+					<a href="/index.php#Product"class=spisok>о нас</a>
+					<a href="/index.php#SovZakaz" class=spisok>совершить заказ</a>
+					<a href="privacy.php"class=spisok>политика конфиденциальности</a>
+					<p class=rejim1>режим работы:</p>
+					<p class=rejim2>Пн - Пт: 08-00 - 19-00</p>
+				</div>
+				<div class=footer_box_03>
+				<a href="https://goo.gl/maps/Phyv8sEE5yUYzc5cA" target="_blank"><img src="image\img\im_dress_01.png" alt="Перейти на карту" title="перейти на карту"></a>
+					<p class=adress>г.Москва, Никольский переулок, д25\2 стр.4 </p>
+				</div>
+			</div>
+		</div>		
+			
+		</article>
+	</body>
+</html>
